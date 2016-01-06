2bbf706fe0742263fa8c3052ea19ade06352c12e
Ivy Wong <llovee91@Joes-MBP.attlocal.net>
2016-01-06 12:52:41 -0800

    changed process.env.PORT

--- app.js
+++ app.js
@@ -64,6 +64,9 @@ app.use('/', shakerRoutes)
 app.use('/', favoriteRoutes)
 
 // start the server on port
-app.listen(port, function(){
-	console.log("Server Running!", port)
-})
+// app.listen(port, function(){
+// 	console.log("Server Running!", port)
+// })
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
